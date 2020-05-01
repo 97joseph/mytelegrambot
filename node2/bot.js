9626e321d806ebc526010422708392d36c780dd8
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-05-01 15:53:29 +0300

    Bot update

---
+++ node2/bot.js
@@ -0,0 +1,21 @@
+const Telegraf=require('telegraf');
+const bot=new Telegraf('token');
+
+//code  /start
+bot.start((ctx)=>{
+   ctx.reply(ctx.from."You have entered the start command");
+   //console.log(ctx)==show all the attributes of the ctx
+   console.log(ctx.from);
+   console.log(ctx.chat);
+   console.log(ctx.message);
+   console.log(ctx.updateSubTypes);
+
+})
+
+bot.help((ctx)=>{
+ ctx.reply("You have entered the help command");   
+})
+bot.settings((ctx)=>{
+    ctx.reply("You have entered the settings command");
+})
+bot.launch(); 
