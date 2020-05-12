d778f82e2d114da4335d4a9a5daa26f7518746d7
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-05-13 01:08:36 +0300

    Main file

--- node2/bot.js
+++ node2/bot.js
@@ -3,7 +3,8 @@ const bot=new Telegraf('token');
 
 //code  /start
 bot.start((ctx)=>{
-   ctx.reply(ctx.from."You have entered the start command");
+   ctx.reply(ctx.from.first_name+"You have entered the start command");
+   ctx.updateSubTypes[0];
    //console.log(ctx)==show all the attributes of the ctx
    console.log(ctx.from);
    console.log(ctx.chat);
